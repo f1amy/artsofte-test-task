5b96d75b07fe5f53d6af26d2fecb68ea8948ea18
Flamy <27062841+f1amy@users.noreply.github.com>
2021-09-17 20:20:07 +0500

    add general api responses

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,32 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Respond with JSON success message.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondApiSuccess()
+    {
+        return response()->json([
+            'Message' => 'Success.',
+        ]);
+    }
+
+    /**
+     * Respond with JSON error message.
+     *
+     * @param string $message
+     * @param int $status
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondApiError(string $message, int $status = 422)
+    {
+        return response()->json([
+            'Errors' => [
+                'Global' => $message,
+            ],
+        ], $status);
+    }
 }
